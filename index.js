f57f6de2bd776baf0cf0cf100dce297f41035d52
amoussard <axel.moussard@gmail.com>
2016-01-08 08:00:35 -0500

    Basic app

---
+++ index.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var request = require('request');
+var bodyParser = require('body-parser');
+var sockets = [];
+
+app.use(express.static('public'));
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/hooks/circle', function(req, res){
+    var project = req.body.payload;
+
+    sockets.forEach(socket => {
+        socket.emit('new_project', {
+            name: project.reponame,
+            branch: project.branch,
+            status: project.status
+        });
+    });
+
+    res.send('ok');
+});
+
+io.on('connection', function(socket) {
+    sockets.push(socket);
+
+    var options = {
+        url: 'http://circleci.com/api/v1/projects?circle-token=435ebb00ad5ab34afc7773ed9c56cf84ea01c8be',
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
+
+    request(options, function(error, response, body) {
+        var projects = JSON.parse(body);
+
+        projects.forEach(project => {
+            for (var branchName in project.branches) {
+                var branch = project.branches[branchName];
+                var lastBuild = branch.recent_builds.shift();
+
+                socket.emit('new_project', {
+                    name: project.reponame,
+                    branch: branchName,
+                    status: lastBuild.status
+                });
+            }
+        });
+    });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
---
+++ public/js/controllers.js
@@ -0,0 +1,59 @@
+var ciViewApp = angular.module('ciViewApp', []);
+
+ciViewApp.factory('socket', function($rootScope) {
+    var socket = io.connect();
+	return {
+		on: function(eventName, callback) {
+			socket.on(eventName, function() {
+				var args = arguments;
+				$rootScope.$apply(function() {
+					callback.apply(socket, args);
+				});
+			});
+		},
+		emit: function(eventName, data, callback) {
+			socket.emit(eventName, data, function() {
+				var args = arguments;
+				$rootScope.$apply(function() {
+					if(callback) {
+						callback.apply(socket, args);
+					}
+				});
+			});
+		}
+	};
+});
+
+var statusToClasses = {
+    'success': 'success',
+    'fixed': 'warning',
+    'failed': 'danger',
+    'canceled': 'info',
+    'timedout': 'info'
+}
+
+ciViewApp.controller('ProjectListCtrl', function ($scope, socket) {
+    $scope.projects = {};
+
+    socket.on('new_project', function(project) {
+        if ($scope.projects[project.name]) {
+            if (!$scope.projects[project.name]['branches'][project.branch]) {
+                $scope.projects[project.name]['branches'][project.branch] = {
+                    name: project.branch
+                };
+            }
+
+            $scope.projects[project.name]['branches'][project.branch]['status'] = statusToClasses[project.status];
+        } else {
+            $scope.projects[project.name] = {
+                name: project.name,
+                branches: {}
+            };
+
+            $scope.projects[project.name]['branches'][project.branch] = {
+                name: decodeURIComponent(project.branch),
+                status: statusToClasses[project.status]
+            };
+        }
+    });
+});
