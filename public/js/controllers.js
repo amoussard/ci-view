a534909eceb51e02cc48843b339f129cafb2dc5c
amoussard <axel.moussard@gmail.com>
2016-01-12 09:06:33 -0500

    Add animation

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -35,7 +35,13 @@ var statusToClasses = {
 ciViewApp.controller('ProjectListCtrl', function ($scope, socket) {
     $scope.projects = {};
 
+    var alert = false;
+
     socket.on('new_project', function(project) {
+        if (project.status === 'failed') {
+            $scope.receiveError();
+        }
+
         if ($scope.projects[project.name]) {
             if (!$scope.projects[project.name]['branches'][project.branch]) {
                 $scope.projects[project.name]['branches'][project.branch] = {
@@ -56,4 +62,37 @@ ciViewApp.controller('ProjectListCtrl', function ($scope, socket) {
             };
         }
     });
+
+    $scope.receiveError = function() {
+        var alertSound = document.getElementById('alertSound');
+        var bombVideo = document.getElementById('bombVideo');
+        var alertImage = document.getElementById('alertImage');
+        var overlay = document.getElementById('overlay');
+
+        if (!alert) {
+            alert = true;
+
+            bombVideo.style.display = 'block';
+            overlay.style.display = 'block';
+            bombVideo.ontimeupdate = function() {
+                if (bombVideo.currentTime > 6) {
+                    alertSound.play();
+                }
+            };
+            bombVideo.onended = function() {
+                bombVideo.style.display = 'none';
+                overlay.style.display = 'none';
+                alertImage.style.display = 'block';
+
+                setTimeout(function() {
+                    alertSound.pause();
+                    alertSound.currentTime = 0;
+                    bombVideo.currentTime = 0;
+                    alertImage.style.display = 'none';
+                    alert = false;
+                }, 30000);
+            };
+            bombVideo.play();
+        }
+    }
 });
